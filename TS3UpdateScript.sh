c1f3c9f8a9d03ee933ce5813345646a41cc6ae94
Naga <admin@kaio.ws>
2015-04-21 10:50:09 +0200

    Increase version number and last edit date

--- TS3UpdateScript.sh
+++ TS3UpdateScript.sh
@@ -14,8 +14,8 @@ exec 5<&0
 # Donations: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=7ZRXLSC2UBVWE
 #
 
-SCRIPT_VERSION="3.11.7.3"
-LAST_EDIT_DATE="2015-03-14"
+SCRIPT_VERSION="3.11.8.3"
+LAST_EDIT_DATE="2015-04-21"
 
 # Time measurement whole script START
 TIME_MEASUREMENT_SCRIPT_START=`date +%s`
